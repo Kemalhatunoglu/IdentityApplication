2ff5e3646f32ce1943395fdfb50d4093a299bc4e
Kemal Hatunoğlu <kemalhatunoglu@gmail.com>
2021-02-09 19:51:36 +0300

    Add project files.

---
+++ NetCoreIdentityApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreIdentityApplication.Data
+{
+    // Identity işlemi yaparken kendi application dp context isminde hazır bir klass açar bu identity işlemlerinin Context klassıdır.
+    // NoT: Bu context klassı IdentityDbContext den instance alır.
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
